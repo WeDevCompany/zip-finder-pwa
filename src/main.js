abfbbaddca418930f1e8f3085de5845cf7dbc88f
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2019-10-09 15:49:53 +0200

    Added the ionic to the vue project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// Important npm install @ionic/vue@0.0.9
+import Ionic from '@ionic/vue'
 
+import "@ionic/core/css/ionic.bundle.css";
+
+Vue.use(Ionic);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
